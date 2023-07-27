8451dc656bdac9424ac8c5ec801310a9cc698414
ranalav <lavkumarsinh.r@webcodegenie.com>
2023-07-27 18:49:35 +0530

    thinking project name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
+  appName: 'rahgir',
   webDir: 'www',
   server: {
     androidScheme: 'https'
